f2d3f57166598d54de809ac0f594dc344b8ca88c
Yossi Cohen <yossico@rafael.co.il>
2023-12-23 20:53:11 +0200

    ref

--- psql.sh
+++ psql.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
 function psql() {
+
     local POSTGRES_IMAGE="postgis"
     local POSTGRES_HOST="localhost"
     local POSTGRES_USER="postgres"
-    docker exec -it ${POSTGRES_IMAGE} bash -c "psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER}"
+    local OSM_DB="osm"
+
+    if [ $# -gt 0 ]; then
+        docker exec ${POSTGRES_IMAGE} bash -c "psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} -d ${OSM_DB} -c '$*'"
+    else
+        docker exec -it ${POSTGRES_IMAGE} bash -c "psql -h ${POSTGRES_HOST} -U ${POSTGRES_USER} -d ${OSM_DB}"
+    fi
 }
 
 psql "$@"
