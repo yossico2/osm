2f4e6e2a6ed8ccd5608c471b932f4aeb210335a5
Yossi Cohen <yossico@rafael.co.il>
2023-12-23 12:20:21 +0200

    ref

--- import-israel-pbf.sh
+++ import-israel-pbf.sh
@@ -6,11 +6,13 @@ function import-pbf() {
     local PBF="data/israel-and-palestine-latest.osm.pbf"
     local PGSQL_HOST="localhost"
     local POSTGRES_USER="postgres"
+    local OSM_DB="osm"
 
     docker run --network=host \
         -v "${PWD}/${DATADIR}:/osm/data" \
         ${OSM2PGSQL_IMAGE} osm2pgsql \
         -H ${PGSQL_HOST} -P 5432 -U ${POSTGRES_USER} \
+        --database ${OSM_DB} \
         -c ${PBF}
 }
 
