86e2fce94e08ed982e8b0c11094e517e89806f5b
Yossi Cohen <yossico@rafael.co.il>
2023-12-28 15:39:49 +0200

    ref

--- run-all.sh
+++ run-all.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 ./run-postgis.sh
+sleep 3.s
 ./init-osm-db.sh
+sleep 1.s
 ./run-external-data.sh
+sleep 1.s
 ./load-israel-pbf.sh
+sleep 1.s
 ./run-map-proxy.sh
 
 echo "open http://localhost:8080/demo"
